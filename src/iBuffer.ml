dff2b052542a4dd6a6ab4784d8ca05ec4732c7db
Chet Murthy <chetsky@gmail.com>
2020-10-16 17:51:13 -0700

    meh
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	modified:   src/iBuffer.ml
    #

--- src/iBuffer.ml
+++ src/iBuffer.ml
@@ -4,24 +4,22 @@ open Event
     fd : Unix.file_descr ;
     buffer : bytes ;
     mutable ofs: int ;
+    mutable lim : int ;
   }
 
   let create ~bufsiz fd =
     assert (bufsiz > 0);
-    { fd = fd ; buffer = Bytes.create bufsiz ; ofs = bufsiz }
+    { fd = fd ; buffer = Bytes.create bufsiz ; ofs = bufsiz ; lim = bufsiz }
 
   let refill ibuf =
-    let rec rerec ofs toread =
-      assert (ofs >= 0) ;
-      assert (toread >= 0) ;
-      assert (ofs < Bytes.length ibuf.buffer) ;
-      assert (toread <= Bytes.length ibuf.buffer) ;
-      let nread = Unix.read ibuf.fd ibuf.buffer ofs toread in
-      if nread = 0 then 0
-      else if nread = toread then ofs+nread
-      else rerec (ofs+nread) (toread-nread) in
     let toread = Bytes.length ibuf.buffer in
-    rerec 0 toread
+    let nread = Unix.read ibuf.fd ibuf.buffer 0 toread in
+      if nread = 0 then 0
+      else begin
+        ibuf.ofs <- 0 ;
+        ibuf.lim <- nread ;
+        nread
+      end
 
   let read_char loop ibuf kont =
     if ibuf.ofs = Bytes.length ibuf.buffer then
@@ -32,7 +30,7 @@ open Event
            if nread = 0 then kont (-1)
            else begin
              let c = Char.code (Bytes.get ibuf.buffer 0) in
-             ibuf.ofs <- 1 ;
+             ibuf.ofs <- 1 + ibuf.ofs;
              kont c
            end
         )
